47b6190134e9b39e303ac589b599f277b3e8a7f7
ephetic <bpcleary@gmail.com>
2015-09-02 14:55:04 -0700

    [wip] add GeoFire utilities

---
+++ app/utils/geo.js
@@ -0,0 +1,63 @@
+import {geo} from '../../config';
+
+/**
+ * Promisify navigator.geolocation.getCurrentPosition
+ * @param  {PositionOptions} opts See [PositionOptions]{@link http://mdn.io/PositionOptions}
+ * @return {Promise}      Resolve receives [Position]{@link http://mdn.io/Position} Object.
+ */
+const getCurrentPosition = (opts) => {
+  return new Promise(
+    (resolve, reject) => navigator.geolocation.getCurrentPosition(
+      resolve, reject, opts
+    )
+  );
+};
+
+/**
+ * Get current location, trying high-accuracy first, then falling back to low-
+ * accuracy after timeout.  May still reject is low-accuracy attempt rejects.
+ * @return {Promise} Resolve receives [Position]{@link http://mdn.io/Position} Object.
+ */
+const getLocation = () => {
+  // try high-accuracy, then fall-back to cell.
+  return getCurrentPosition({
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 600000,
+  })
+  .catch(getCurrentPosition({
+    enableHighAccuracy: false,
+    timeout: 5000,
+    maximumAge: 600000,
+  }));
+};
+
+/**
+ * Private reference to most recently executed GeoFire query.
+ */
+let cancelQuery;
+
+/**
+ * Register handler callback for all items found within given radius.
+ * @param  {Number}   radius
+ * @param  {function} handler Will be passed a {key:location} instance for every item found.
+ * @return {function}         Call to cancel all callbacks on the current query.
+ */
+export const onSitesWithinRadius = (radius, handler) => {
+  getLocation().then(({coords: {latitude, longitude}}) => {
+    const center = [latitude, longitude];
+    cancelQuery && cancelQuery();
+    const query = geo.query({center, radius});
+    query.on('key_entered', handler);
+    cancelQuery = () => query.cancel();
+    return cancelQuery;
+  });
+};
+
+/**
+ * Add site location to GeoFire table.
+ * @param  {String} siteId      Firebase key String
+ * @param  {[Number, Number]}   [latitude, longitude]
+ * @return {Promise}            Then callback called when data synchronized with Firebase.
+ */
+export const addSiteLocation = (siteId, [latitude, longitude]) => geo.set(siteId, [latitude, longitude]);
