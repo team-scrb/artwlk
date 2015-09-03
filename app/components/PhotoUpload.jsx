edce582d38406bd1a0e57a73c4fde8c585e2742e
Rodolfo Yabut <rodolfoyabut@gmail.com>
2015-09-03 14:52:15 -0700

    [bug] fix base64 header parsing for photo

--- app/components/PhotoUpload.jsx
+++ app/components/PhotoUpload.jsx
@@ -16,16 +16,16 @@ export default class PhotoUpload extends React.Component {
         method: 'post',
         url: 'https://api.imgur.com/3/image',
         headers: {
-          Authorization: 'Client-ID b2670fcbac651a9',
+          Authorization: 'Client-ID 6b87d72f0811d55',
         },
         data: {
-          image: encodedImage.target.result.split('data:image/png;base64,')[1],
+          image: encodedImage.target.result.split(',')[1],
           type: 'base64',
         },
       })
       .then((response) => {
         // This is the link to the photo
-        console.log('IMGUR URL: ', response.data.data.link);
+        window.location.replace(response.data.data.link);
       })
       .catch((err) => {
         console.error(err);
