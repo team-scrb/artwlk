e35b7a9a8950c20d29fe58567f79cc47a167712d
ephetic <bpcleary@gmail.com>
2015-09-04 10:27:59 -0700

    [wip] trying to get firebase going with phantom

--- karma.conf.js
+++ karma.conf.js
@@ -2,18 +2,20 @@ module.exports = function karmaConfig(config) {
   config.set({
     basePath: '.',
     frameworks: ['jasmine'],
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
+    client: {
+      captureConsole: true,
+    },
     files: [
       'node_modules/es5-shim/es5-shim.js',
       'node_modules/es6-promise-polyfill/promise.js',
       'spec/SpecHelper.js',
       'spec/**/*Spec.js*',
     ],
-
+    logLevel: config.LOG_DEBUG,
     preprocessors: {
       'spec/**/*Spec.js*': ['webpack'],
     },
-
     webpack: require('./webpack.config.js'),
     webpackServer: { noInfo: true },
     singleRun: true,
