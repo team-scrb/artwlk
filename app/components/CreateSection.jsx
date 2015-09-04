d52de0c66f3fe9060b4f1cfec8b89a92192e2b0b
Chris Ha <chriskevinha@gmail.com>
2015-09-03 17:55:22 -0700

    [feat] add flexbox styles

--- app/components/CreateSection.jsx
+++ app/components/CreateSection.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import PhotoUpload from './PhotoUpload';
 
+// styles
+import '../styles/components/CreateSection';
+
 export default class CreateSection extends React.Component {
   render() {
     return (
-      <div>
+      <div className="CreateSection">
         <h2>Create Site Here</h2>
-        <PhotoUpload />
+        <form>
+          <PhotoUpload />
+          <button className="CreateSection__selectLocationBtn">Select Location</button>
+          <label>Name
+            <input type="text" name="name" />
+          </label>
+          <label>Artist
+            <input type="text" name="artist" />
+          </label>
+          <label>Category</label>
+          <input type="checkbox" name="category" value="Street Art" />Street Art
+          <input type="checkbox" name="category" value="Architecture" />Architecture
+          <label>Tags
+            <input type="text" name="tags" />
+          </label>
+          <label>Description
+            <input type="text" name="description" />
+          </label>
+          <input type="submit" />
+        </form>
       </div>
     );
   }
--- app/components/TourSection.jsx
+++ app/components/TourSection.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
 
+// styles
+import '../styles/components/TourSection';
+
 export default class Tour extends React.Component {
   render() {
     return (
-      <h2>Make me a tour please!</h2>
+      <div className="TourSection">
+        <h2>Make me a tour please!</h2>
+        <ul>
+          <li>
+            <img src="" />
+            <h3>Title</h3>
+            <div>Rating</div>
+            <span>Categories, Categories</span>
+            <span>Total Distance</span>
+            <span># of sites</span>
+          </li>
+          <li>
+            <img src="" />
+            <h3>Title</h3>
+            <div>Rating</div>
+            <span>Categories, Categories</span>
+            <span>Total Distance</span>
+            <span># of sites</span>
+          </li>
+        </ul>
+      </div>
     );
   }
 }
