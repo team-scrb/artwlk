460d2ede09ea2994a693b7ac66db45911ab65bcf
ephetic <bpcleary@gmail.com>
2015-09-04 19:24:31 -0700

    [bug] fix installer to force ln

--- utils/installGitHooks.sh
+++ utils/installGitHooks.sh
@@ -3,5 +3,5 @@
 chmod a+x utils/pre-commit
 chmod a+x utils/pre-push
 
-ln utils/pre-commit .git/hooks/pre-commit
-ln utils/pre-push .git/hooks/pre-push
+ln -f utils/pre-commit .git/hooks/pre-commit
+ln -f utils/pre-push .git/hooks/pre-push
