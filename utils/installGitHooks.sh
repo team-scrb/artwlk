7c0bf0c9060e04291e039b6da0bce4e4208e831a
ephetic <bpcleary@gmail.com>
2015-09-10 16:44:18 -0700

    [wip] add post-rebase hook

--- utils/installGitHooks.sh
+++ utils/installGitHooks.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 chmod a+x utils/pre-commit
-chmod a+x utils/pre-push
-
 ln -f utils/pre-commit .git/hooks/pre-commit
+
+chmod a+x utils/pre-push
 ln -f utils/pre-push .git/hooks/pre-push
+
+chmod a+x utils/post-rebase
+ln -f utils/post-rebase .git/hooks/post-rebase
