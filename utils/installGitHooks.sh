292006e0a0c1605a5622926ee3fcd0079bbb5be9
ephetic <bpcleary@gmail.com>
2015-09-10 16:55:31 -0700

    [bug] post-rebase not a hook.  trying just rebase

--- utils/installGitHooks.sh
+++ utils/installGitHooks.sh
@@ -6,5 +6,5 @@ ln -f utils/pre-commit .git/hooks/pre-commit
 chmod a+x utils/pre-push
 ln -f utils/pre-push .git/hooks/pre-push
 
-chmod a+x utils/post-rebase
-ln -f utils/post-rebase .git/hooks/post-rebase
+chmod a+x utils/rebase
+ln -f utils/rebase .git/hooks/rebase
